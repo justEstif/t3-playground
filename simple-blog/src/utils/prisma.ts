b41c9896899a079e46299da32d4882322dc43113
Estifanos <estifanosbeyene21@gmail.com>
2022-09-30 11:15:22 -0400

    feat: move prisma file to utils folder

--- simple-blog/src/utils/prisma.ts
+++ simple-blog/src/utils/prisma.ts
@@ -1,12 +1,14 @@
-// src/server/db/client.ts
+// NOTE prisma client file
 import { PrismaClient } from "@prisma/client";
-import { env } from "../../env/server.mjs";
+import { env } from "../env/server.mjs";
 
+// NOTE to prevent active instance warnings in editor
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
+// use prisma that is current active or create new
 export const prisma =
   global.prisma ||
   new PrismaClient({
@@ -14,6 +16,7 @@ export const prisma =
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
 
+// stop prisma from creating new instances
 if (env.NODE_ENV !== "production") {
   global.prisma = prisma;
 }
