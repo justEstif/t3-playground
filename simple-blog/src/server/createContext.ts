9d5b61f73be326acd8be5e8e883d213efa5c8a11
Estifanos <estifanosbeyene21@gmail.com>
2022-09-30 11:15:48 -0400

    feat: add prisma to context

--- simple-blog/src/server/createContext.ts
+++ simple-blog/src/server/createContext.ts
@@ -1,11 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../utils/prisma";
 
 type TContext = {
   req: NextApiRequest;
   res: NextApiResponse;
 };
 
-export const createContext = ({ req, res }: TContext) => ({ req, res });
+export const createContext = ({ req, res }: TContext) => ({
+  req,
+  res,
+  prisma,
+});
 
 export type Context = ReturnType<typeof createContext>;
-
