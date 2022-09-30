a8fc6cdaed4004523799ea6e3a890f6f23ebc9d9
Estifanos <estifanosbeyene21@gmail.com>
2022-09-30 11:49:31 -0400

    feat: add user create input validator

---
+++ simple-blog/src/schema/user.schema.ts
@@ -0,0 +1,14 @@
+// create user schema
+import z from "zod"; // validation library
+
+export const createUserSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+});
+
+export const createUserOutputSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+});
+
+export type CreateUserInput = z.TypeOf<typeof createUserSchema>;
